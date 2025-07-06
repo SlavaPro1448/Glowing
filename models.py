87fecb5c84396ea4b04e0b8f2089bd556cca524c
SlavaPro1448 <140442168+SlavaPro1448@users.noreply.github.com>
2025-07-06 19:20:07 +0100

    v18

---
+++ models.py
@@ -0,0 +1,21 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+
+db = SQLAlchemy()
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
+    role = db.Column(db.String(20), nullable=False)  # 'admin' или 'operator'
+    assigned_operator_name = db.Column(db.String(64))
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def is_admin(self):
+        return self.role == 'admin'
